ce120d9f4b000b6bee74ffa3916edce92b99e967
poblos <zoltykropek@gmail.com>
2022-09-04 18:37:29 +0200

    Income chart added to budget menu

--- src/main/java/com/example/demo/BudgetController.java
+++ src/main/java/com/example/demo/BudgetController.java
@@ -1,18 +1,26 @@
 package com.example.demo;
 
 import com.example.demo.budget.ExpenseCategory;
+import com.example.demo.budget.IncomeCategory;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
 
 public class BudgetController {
     @FXML
-    private PieChart chart;
+    private ToggleGroup incomeOrExpenses;
+    @FXML private RadioButton incomeButton;
+    @FXML private RadioButton expensesButton;
     @FXML
-    private VBox budgetBox;
+    private PieChart chart;
+
     private MainController mainController;
 
     public void setMainController(MainController mainController) {
@@ -20,12 +28,30 @@ public class BudgetController {
     }
 
     public void update() {
+        if (incomeOrExpenses.getSelectedToggle() == expensesButton) {
+            onExpenseButtonClick();
+        } else {
+            onIncomeButtonClick();
+        }
+    }
+
+    public void onExpenseButtonClick() {
         ObservableList<PieChart.Data> pieChartData =
                 FXCollections.observableArrayList();
-        for (ExpenseCategory category: ExpenseCategory.values()) {
-            pieChartData.add(new PieChart.Data(category.toString(),mainController.getGame().getBudget().getExpenses().get(category)));
+        for (ExpenseCategory category : ExpenseCategory.values()) {
+            pieChartData.add(new PieChart.Data(category.toString(), mainController.getGame().getBudget().getExpenses().get(category)));
+            chart.setTitle("Expenses");
         }
         chart.setData(pieChartData);
-        chart.setTitle("Expenses");
+    }
+
+    public void onIncomeButtonClick() {
+        ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList();
+        for (IncomeCategory category : IncomeCategory.values()) {
+            pieChartData.add(new PieChart.Data(category.toString(), mainController.getGame().getBudget().getIncome().get(category)));
+            chart.setTitle("Income");
+        }
+        chart.setData(pieChartData);
     }
 }
