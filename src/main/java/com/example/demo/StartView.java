e5299702f01158762d9cc7ee5c59b5c92bd15618
poblos <zoltykropek@gmail.com>
2022-10-04 20:50:13 +0200

    Added modifier conditions for Graonian events

--- src/main/java/com/example/demo/StartView.java
+++ src/main/java/com/example/demo/StartView.java
@@ -27,14 +27,11 @@ import com.example.demo.policy.PolicyChange;
 import com.example.demo.policy.PolicyCondition;
 import com.squareup.moshi.Moshi;
 import com.squareup.moshi.adapters.PolymorphicJsonAdapterFactory;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
