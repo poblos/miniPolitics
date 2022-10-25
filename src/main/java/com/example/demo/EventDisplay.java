dc20044d52c6759c70521adcb33e0f1b2530c0f5
poblos <zoltykropek@gmail.com>
2022-10-25 22:20:55 +0200

    Made general style more colourful.

--- src/main/java/com/example/demo/EventDisplay.java
+++ src/main/java/com/example/demo/EventDisplay.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import com.example.demo.event.Event;
 import com.example.demo.event.Option;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
@@ -9,6 +10,8 @@ import javafx.scene.text.Font;
 
 public class EventDisplay extends VBox {
     public EventDisplay(Event event, MainController controller) {
+        this.setFillWidth(true);
+
         // Title setup
         Label title = new Label();
         title.setText(event.getTitle());
@@ -29,13 +32,14 @@ public class EventDisplay extends VBox {
             EventButton button = new EventButton(i);
             button.setText(option.getDescription());
             button.setOnAction(actionEvent-> controller.handleEvent(button.getClick()));
+            button.setMaxWidth(1000);
             this.getChildren().add(button);
             i++;
         }
 
-        this.setStyle("        -fx-border-color:linear-gradient(from 25px 1px to 100px 100px, reflect, #00A2E8, #F9F9F9);" +
+        this.setStyle("" +
                 "        -fx-border-radius:10;" +
                 "        -fx-border-width:4;" +
-                "-fx-background-color: linear-gradient(from 25px 1px to 100px 100px,#00A2E8, #F9F9F9)");
+                "-fx-background-color: linear-gradient(from 50px 0px to 50px 50px, lightgrey, deepskyblue)");
     }
 }
