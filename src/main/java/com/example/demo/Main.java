f9f08800f1233850a80f02cb0e2c41d35bf09357
poblos <zoltykropek@gmail.com>
2022-10-29 12:42:18 +0200

    Added proper start menu

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("start-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("event.css")).toExternalForm());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("start-view.css")).toExternalForm());
         stage.setTitle("miniPolitics");
         stage.setScene(scene);
         stage.initStyle(StageStyle.TRANSPARENT);
