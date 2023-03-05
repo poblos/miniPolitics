aa3ab5b3c5697bd7ba0d4698df29daf190adc81d
poblos <zoltykropek@gmail.com>
2023-03-05 02:15:41 +0100

    Made JobChoice follow the new standards of events; added event about reimbursed meedicines

--- src/main/java/com/example/demo/JobChoiceDisplay.java
+++ src/main/java/com/example/demo/JobChoiceDisplay.java
@@ -4,6 +4,8 @@ import com.example.demo.jobs.Job;
 import com.example.demo.jobs.Person;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
@@ -12,15 +14,21 @@ public class JobChoiceDisplay extends VBox {
         // Title setup
         Label title = new Label();
         title.setText("Choose job");
-        title.setFont(new Font("Serif", 28));
+        title.getStyleClass().add("eventTitle");
         this.getChildren().add(title);
 
         // Desc setup
         TextArea desc = new TextArea();
         desc.setText("Choose job for:" + person);
+        desc.setTextFormatter(new TextFormatter<String>(change -> {
+            change.setAnchor(change.getCaretPosition());
+            return change;
+        }));
+        desc.setWrapText(true);
         desc.setEditable(false);
-        desc.setFont(new Font("Serif", 16));
         desc.setPrefRowCount(2);
+        desc.setPrefWidth(600);
+        desc.getStyleClass().add("eventDesc");
         this.getChildren().add(desc);
 
         //Jensons setup
@@ -28,7 +36,9 @@ public class JobChoiceDisplay extends VBox {
             JobButton button = new JobButton(job);
             button.setText(String.valueOf(job));
             button.setOnAction(actionEvent -> controller.handleJobChoice(button.getJob()));
-            button.setPrefWidth(900);
+            button.setPrefWidth(600);
+            button.setMaxHeight(100);
+            button.getStyleClass().add("eventButton");
             this.getChildren().add(button);
         }
 
