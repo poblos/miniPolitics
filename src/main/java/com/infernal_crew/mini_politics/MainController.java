b426d773500d81772e9dcc9b3e0c668c75b09791
poblos <zoltykropek@gmail.com>
2023-10-15 17:40:37 +0200

    BUGFIX: InfrustructureCorruption excluded from lose conditions

--- src/main/java/com/infernal_crew/mini_politics/MainController.java
+++ src/main/java/com/infernal_crew/mini_politics/MainController.java
@@ -74,7 +74,7 @@ public class MainController {
 
         // Game is lost
         for(Indicator ind : Indicator.values()) {
-            if (game.getIndicatorValue(ind) < 0) {
+            if (game.getIndicatorValue(ind) < 0 && ind != Indicator.InfrastructureCorruption) {
                 eventBox.getChildren().clear();
                 eventBox.getChildren().add(new EventDisplay(game.getLoseEvent(ind), this));
                 return;
