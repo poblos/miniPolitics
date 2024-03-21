4e344f3912387750aa5db7e3de7b6beefb6df762
poblos <zoltykropek@gmail.com>
2024-03-21 15:57:58 +0100

    Descriptions below indicators replaced by tooltips; descriptions are still to be written

--- src/main/java/com/infernal_crew/mini_politics/components/IndicatorDisplay.java
+++ src/main/java/com/infernal_crew/mini_politics/components/IndicatorDisplay.java
@@ -2,27 +2,34 @@ package com.infernal_crew.mini_politics.components;
 
 import com.infernal_crew.mini_politics.game.Game;
 import com.infernal_crew.mini_politics.indicators.Indicator;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Tooltip;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.util.Duration;
 
 import java.util.Objects;
 
 
 public class IndicatorDisplay extends VBox {
-    Label text;
     Label graphic;
     ProgressBar bar;
     String name;
+    Tooltip tooltip;
 
     public IndicatorDisplay(String name) {
-        this.setAlignment(Pos.CENTER);
+        this.setAlignment(Pos.TOP_CENTER);
+        this.setPadding(new Insets(10));
+        this.setSpacing(10);
         this.name = name;
-        this.text = new Label(this.name);
         this.bar = new ProgressBar();
         try {
 
@@ -33,9 +40,24 @@ public class IndicatorDisplay extends VBox {
             System.out.println("problem");
         }
         this.getChildren().add(graphic);
-        this.getChildren().add(text);
         this.getChildren().add(bar);
         this.setEffect(new DropShadow());
+
+        tooltip = new Tooltip();
+        Text firstPart = new Text("Party Cohesion\n");
+        firstPart.setFill(Color.YELLOW);
+        Text secondPart = new Text("represents the general sentiment in our party; the actions we take against politicians of our own party lower it (for example if we want to tackle corruption or make decisions that are against views of some of our party members, or favor people that aren't in our party for long time); on the contrary, the actions such as covering up for our MPs or listening to the old wing of our party increases it.");
+        secondPart.setFill(Color.WHITE);
+        TextFlow textFlow = new TextFlow(firstPart, secondPart);
+
+        tooltip.setGraphic(textFlow);
+
+        tooltip.setWrapText(true);
+        tooltip.setMaxWidth(200);
+
+        Tooltip.install(this.graphic, tooltip);
+        tooltip.setShowDelay(Duration.millis(500));
+
 
     }
 
