8b94e2dff15fed9a555cfd4a3bd63a9620d2ee4c
poblos <zoltykropek@gmail.com>
2024-07-12 22:54:05 +0200

    Gas deal chain of events expanded

--- src/main/java/com/infernal_crew/mini_politics/controllers/StoryController.java
+++ src/main/java/com/infernal_crew/mini_politics/controllers/StoryController.java
@@ -1,43 +1,47 @@
 package com.infernal_crew.mini_politics.controllers;
 
-import com.infernal_crew.mini_politics.game.Game;
 import com.infernal_crew.mini_politics.story.StoryNote;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
-import static com.infernal_crew.mini_politics.utils.UICommon.*;
+import static com.infernal_crew.mini_politics.utils.UICommon.addHighlightedText;
+import static com.infernal_crew.mini_politics.utils.UICommon.addNormalText;
 
 public class StoryController extends BarController {
     @FXML
-    private VBox storyBox;
+    private ListView<StoryNote> storyListView;
     @FXML
     private TextFlow title;
-
     @FXML
     private TextFlow description;
 
     @Override
     public void update() {
-        storyBox.getChildren().clear();
-        title.getChildren().clear();
-        description.getChildren().clear();
-
-        for(StoryNote note : mainController.getGame().getStoryNotes().values()) {
-            if(!note.isShown()) continue;
-            CheckBox checkBox = new CheckBox();
-            checkBox.setDisable(true);
-            checkBox.setStyle("-fx-opacity: 1");
-            checkBox.setSelected(note.isDone());
-
-            addHighlightedClickableText(storyBox, note.getTitle(), event-> showDetails(note));
-            storyBox.getChildren().add(checkBox);
-
+        ObservableList<StoryNote> storyNotes = FXCollections.observableArrayList();
+        for (StoryNote note : mainController.getGame().getStoryNotes().values()) {
+            if (note.isShown()) {
+                storyNotes.add(note);
+            }
         }
+        storyListView.setItems(storyNotes);
+        storyListView.setCellFactory(param -> new StoryNoteListCell());
+
+        storyListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                showDetails(newValue);
+            } else {
+                title.getChildren().clear();
+                description.getChildren().clear();
+            }
+        });
     }
 
     private void showDetails(StoryNote note) {
@@ -45,5 +49,26 @@ public class StoryController extends BarController {
         description.getChildren().clear();
         addHighlightedText(title, note.getTitle());
         addNormalText(description, note.getDescription());
+    }
+
+    static class StoryNoteListCell extends ListCell<StoryNote> {
+        @Override
+        protected void updateItem(StoryNote item, boolean empty) {
+            super.updateItem(item, empty);
+            if (empty || item == null) {
+                setText(null);
+                setGraphic(null);
+            } else {
+                TextFlow textFlow = new TextFlow();
+                addHighlightedText(textFlow,item.getTitle());
+                setGraphic(textFlow);
+
+                CheckBox checkBox = new CheckBox();
+                checkBox.setDisable(true);
+                checkBox.setStyle("-fx-opacity: 1");
+                checkBox.setSelected(item.isDone());
+                setGraphic(new HBox(10, checkBox, textFlow));
+            }
+        }
     }
 }
